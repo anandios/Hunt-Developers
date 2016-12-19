f339d9e4db306a912f60ddfcd0df4919cd486385
Anand Singh <anand.singh@cobi.bike>
2016-12-19 06:26:40 +0200

    Extracted `ConcreteUserListViewModel`

---
+++ Hunt Developers/ViewControllers/List/ConcreteUserListViewModel.swift
@@ -0,0 +1,67 @@
+//
+//  ConcreteUserListViewModel.swift
+//  Hunt Developers
+//
+//  Created by Anand Rahul Singh on 12/19/16.
+//  Copyright © 2016 NoOrg. All rights reserved.
+//
+
+import Foundation
+
+class ConcreteUserListViewModel : NSObject, UserListViewModel {
+    var delegate: UserListViewModelDelegate? = nil
+    var title: String
+    let appCore: AppCore
+    var users: [UserObject] = Array()
+    
+    var itemSelectedCommand:((_ index: Int) -> ())?
+    
+    init(appCore: AppCore) {
+        self.appCore = appCore
+        self.appCore.useCases.getUsersUseCase?.getUsers()
+        self.title = "Java Developers"
+        super.init()
+        self.appCore.useCases.getUsersUseCase?.delegate = self
+        
+        if let users = self.appCore.useCases.getUsersUseCase?.users {
+            self.users = convertUsers(users)
+            
+            if self.users.count > 0 {
+                delegate?.datasourceUpdate()
+            } else {
+                delegate?.noUpdatesLoaded()
+            }
+        }
+    }
+    
+    public func didSelectItem(_ index: Int) {
+        if let block = itemSelectedCommand {
+            block(index)
+        }
+    }
+    
+    public func didReachLastItem() {
+        self.appCore.useCases.getUsersUseCase?.getUsers()
+    }
+    
+    func convertUsers(_ users:[User]) -> [UserObject] {
+        var usersArray:[UserObject] = Array()
+        
+        for user in users {
+            let userObj = UserObject.init(name: user.name, userName: user.userName, profileImageUrl: user.profileImageUrl, joinedAt: user.joinedAt)
+            usersArray.append(userObj)
+        }
+        return usersArray
+    }
+}
+
+extension ConcreteUserListViewModel : GetUsersDelegate {
+    func didLoadUsers(_ users: [User]) {
+        self.users = convertUsers(users)
+        if self.users.count > 0 {
+            delegate?.datasourceUpdate()
+        } else {
+            delegate?.noUpdatesLoaded()
+        }
+    }
+}
